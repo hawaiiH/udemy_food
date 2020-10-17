90e680d8288668dea79f35fc45c3494822148e93
hawaiiH <vladgusakov@gmail.com>
2020-10-17 22:51:33 +0300

    food

---
+++ js/script.js
@@ -0,0 +1,29 @@
+import calc from './modules/calc';
+import cards from './modules/cards';
+import forms from './modules/forms';
+import modal from './modules/modal';
+import slider from './modules/slider';
+import tabs from './modules/tabs';
+import timer from './modules/timer';
+import {openModal} from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', function() {
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
+
+    calc();
+    cards();
+    forms('form', modalTimerId);
+    modal('[data-modal]', '.modal', modalTimerId);
+    slider({
+        container: '.offer__slider',
+        slide: '.offer__slide',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        tatalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    timer('.timer', '2020-11-11');
+});
